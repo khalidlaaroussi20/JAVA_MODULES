7b0a17cc0f48c2dbb16fa4965f869916410f2f81
Khalid Laaroussi <klaarous@e1r1p11.1337.ma>
2023-06-22 16:29:05 +0100

    add ex00 & ex01 Module 00

--- Module00/ex01/Program.java
+++ Module00/ex01/Program.java
@@ -1,7 +1,52 @@
 package ex01;
 
-class Program{
-    public static void main(String[] args) {
 
+import java.util.Scanner;
+import java.util.InputMismatchException;
+
+class Program {
+
+    public static  class  PrimeNumberInfos {
+        boolean isPrime = false;
+        int     numberIteration = 0;
+
+        PrimeNumberInfos(boolean isPrime, int     numberIteration ) {
+            this.isPrime = isPrime;
+            this.numberIteration = numberIteration;
+        }
+
+        @Override
+        public String toString() {
+            return isPrime  + " " + numberIteration;
+        }
+
+
+    }
+    private static final int PRIME_NUMBER = 0;
+    private static PrimeNumberInfos isPrime(int number) {
+        int sqrtNumber = (int)Math.sqrt(number);
+
+        for (int  i = 2; i <= sqrtNumber; i++) {
+            if (number % i == 0) {
+                return new PrimeNumberInfos(false, i - 1);
+            }
+        }
+        return (new PrimeNumberInfos(true, sqrtNumber));
+    }
+    public static void main(String[] args) {
+        try {
+            Scanner scanner = new Scanner(System.in);
+
+            // Read an integer from the user
+            System.out.print("Enter an integer: ");
+            int number = scanner.nextInt();
+            if (number <= 1)
+                throw new InputMismatchException();
+
+            PrimeNumberInfos primeNumberInfos = isPrime(number);
+            System.out.println(primeNumberInfos);
+        } catch (InputMismatchException e) {
+            System.out.println("IllegalArgument");
+        }
     }
 }
